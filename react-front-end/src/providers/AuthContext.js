af8b5292fbf8b58364687d7bc9863d25f54cff47
David Zhou <d33zhou@outlook.com>
2022-02-14 18:19:03 -0500

    add login and logout functions for auth context

--- react-front-end/src/providers/AuthContext.js
+++ react-front-end/src/providers/AuthContext.js
@@ -1,21 +1,31 @@
 import { createContext, useState } from 'react';
 
-const authContext = createContext();
+export const authContext = createContext();
 
-const AuthProvider = props => {
+export const AuthProvider = props => {
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
 
+  // save user details to state
+  const login = (email, password) => {
+    setAuth(true); // to do some authentication
+    const id = "1234"; // set an ID to be returned from the DB
+    setUser({ id, first_name, last_name, email, avatar });
+  };
+
+  // remove all saved state
+  const logout = () => {
+    setAuth(false);
+    setUser(null);
+  };
+
+  // functions to expose through authContext
   const userData = { auth, user, login, logout };
 
+  // to wrap components to share context
   return (
     <authContext.Provider value={userData}>
       {props.children}
     </authContext.Provider>
   );
-};
-
-export default {
-  authContext,
-  AuthProvider,
 };
