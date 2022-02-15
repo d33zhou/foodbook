a6356aeb331f08d427c9874e5d5618f8cf60f4a1
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-02-14 19:45:31 -0500

    Add 404 route

--- react-front-end/src/components/App.jsx
+++ react-front-end/src/components/App.jsx
@@ -1,104 +1,107 @@
-import axios from 'axios';
+import axios from "axios";
 import {
   Box,
   Container,
   CssBaseline,
   ScopedCssBaseline,
   Typography,
-} from '@mui/material';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+} from "@mui/material";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-import ContainerLogin from './ContainerLogin';
-import SearchAppBar from './SearchAppBar';
-import Navigation from './Navigation';
-import RecipeList from './RecipeList';
-import RecipeFilters from './RecipeFilters';
-import RecipeForm from './RecipeForm';
-import RecipeItem from './RecipeItem';
+import ContainerLogin from "./ContainerLogin";
+import SearchAppBar from "./SearchAppBar";
+import Navigation from "./Navigation";
+import RecipeList from "./RecipeList";
+import RecipeFilters from "./RecipeFilters";
+import RecipeForm from "./RecipeForm";
+import RecipeItem from "./RecipeItem";
 
 function App() {
   return (
     <Router>
-      <Switch>
-    <>
-    
       <CssBaseline />
       <Box
-        className='App'
+        className="App"
         sx={{
-          backgroundColor: '#f0f2f5',
-          height: '100%',
-        }}>
+          backgroundColor: "#f0f2f5",
+          height: "100%",
+        }}
+      >
+        <Switch>
           <Route exact path="/">
-        <Container maxWidth='lg'>
-          <ContainerLogin />
-        </Container>
-        </Route>
-        <Route path="/feed">
-        <SearchAppBar />
-        <Box
-          maxWidth='lg'
-          mx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'flex-start',
-            columnGap: '4rem',
-            margin: '0 auto',
-          }}>
-          <Navigation />
-          <Box>
-            <RecipeList />
-          </Box>
-          <RecipeFilters />
-        </Box>
-        </Route>
-        <Route path="/create">
-        {/* Recipe Form */}
-        <Container maxWidth='sm'>
-          <h2>Recipe Form Component</h2>
-          <Box
-          maxWidth='lg'
-          mx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'flex-start',
-            columnGap: '4rem',
-            margin: '0 auto',
-          }}>
-          <Navigation />
-          
-          <RecipeForm />
-          </Box>
-        </Container>
-        </Route>
-        <Route path="/recipe/:id">
-        {/* Recipe Item */}
-        <Container maxWidth='md'>
-          <h2>Recipe Item Component</h2>
-          <Box
-          maxWidth='lg'
-          mx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'flex-start',
-            columnGap: '4rem',
-            margin: '0 auto',
-          }}>
-          <Navigation />
-          <RecipeItem />
-          </Box>
-        </Container>
-        </Route>
-        
+            <Container maxWidth="lg">
+              <ContainerLogin />
+            </Container>
+          </Route>
+          <Route path="/feed">
+            <SearchAppBar />
+            <Box
+              maxWidth="lg"
+              mx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "flex-start",
+                columnGap: "4rem",
+                margin: "0 auto",
+              }}
+            >
+              <Navigation />
+              <Box>
+                <RecipeList />
+              </Box>
+              <RecipeFilters />
+            </Box>
+          </Route>
+          <Route path="/create">
+            {/* Recipe Form */}
+            <Container maxWidth="sm">
+              <h2>Recipe Form Component</h2>
+              <Box
+                maxWidth="lg"
+                mx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "flex-start",
+                  columnGap: "4rem",
+                  margin: "0 auto",
+                }}
+              >
+                <Navigation />
+
+                <RecipeForm />
+              </Box>
+            </Container>
+          </Route>
+          <Route path="/recipe/:id">
+            {/* Recipe Item */}
+            <Container maxWidth="md">
+              <h2>Recipe Item Component</h2>
+              <Box
+                maxWidth="lg"
+                mx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "flex-start",
+                  columnGap: "4rem",
+                  margin: "0 auto",
+                }}
+              >
+                <Navigation />
+                <RecipeItem />
+              </Box>
+            </Container>
+          </Route>
+
+          <Route path="/404" render={() => <h2>404 - Not Found</h2>} />
+          <Redirect to="/404" />
+        </Switch>
       </Box>
-      {/* <Route path="*">
-                <h2>404 - Not Found</h2>
-              </Route> */}
-      
-    </>
-    
-    </Switch>
     </Router>
   );
 }
