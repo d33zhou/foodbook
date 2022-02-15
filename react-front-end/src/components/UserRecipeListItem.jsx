ba7536c52c2f5d545d0ce7e74ddb9fbfa578cc05
David Zhou <d33zhou@outlook.com>
2022-02-15 02:10:49 -0500

    update sizing properties for image list

--- react-front-end/src/components/UserRecipeListItem.jsx
+++ react-front-end/src/components/UserRecipeListItem.jsx
@@ -1,14 +1,18 @@
-import { IconButton, ImageListItem, ImageListItemBar } from '@mui/material';
+import { IconButton, ImageListItem, ImageListItemBar, Box } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 
 const UserRecipeListItem = (props) => {
   const { image_link, title, creator_id, first_name, last_name } = props;
 
   return (
-    <ImageListItem>
-      <img
+    <ImageListItem rows={1}>
+      <Box
+        component="img"
+        sx={{
+          height: 164,
+          width: 1/1
+        }}
         src={`${image_link}?w=164&h=164&fit=crop&auto=format`}
-        srcSet={`${image_link}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
       />
       <ImageListItemBar
         title={title}
