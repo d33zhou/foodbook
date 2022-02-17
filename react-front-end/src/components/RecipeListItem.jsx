a19e9eb850ef39684846e15e5afdd053dea959ba
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-02-17 10:06:20 -0500

    Add icons for each RecipeListItem

--- react-front-end/src/components/RecipeListItem.jsx
+++ react-front-end/src/components/RecipeListItem.jsx
@@ -1,9 +1,13 @@
-import { Box, Chip, Stack, Typography } from '@mui/material';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import DinnerDiningOutlinedIcon from '@mui/icons-material/DinnerDiningOutlined';
-import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import { Box, Chip, Stack, Typography } from "@mui/material";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import DinnerDiningOutlinedIcon from "@mui/icons-material/DinnerDiningOutlined";
+import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
 
 const RecipeListItem = (props) => {
   const {
@@ -16,16 +20,22 @@ const RecipeListItem = (props) => {
     restrictions,
   } = props;
   // console.log(id);
+  
+  const onClick = () => {
+    console.log(id);
+
+  }
   return (
     <Box
       sx={{
-        textAlign: 'left',
-        paddingRight: '4rem',
-      }}>
+        textAlign: "left",
+        paddingRight: "4rem",
+      }}
+    >
       <Link to={`/recipe/${id}`}>
-        <img src={image_link} alt='' />
+        <img src={image_link} alt="" />
       </Link>
-      <Stack direction='row' spacing={1}>
+      <Stack direction="row" spacing={1}>
         <Chip icon={<DinnerDiningOutlinedIcon />} label={`${cuisine}`} />
         <Chip icon={<AccessTimeOutlinedIcon />} label={`${prepTime} minutes`} />
         {restrictions && (
@@ -34,12 +44,31 @@ const RecipeListItem = (props) => {
             label={restrictions}
           />
         )}
+        <FavoriteBorderOutlinedIcon
+          fontSize="medium"
+          onClick={onClick}
+          sx={{
+            "&:hover": {
+              color: "orangered",
+            },
+            
+          }}
+        />
+        <BookmarkBorderOutlinedIcon
+          fontSize="medium"
+          color="primary"
+          sx={{
+            "&:hover": {
+              color: "orangered",
+            },
+          }}
+        />
       </Stack>
       <Link to={`/recipe/${id}`}></Link>
-      <Typography variant='h3' color='primary' fontWeight='bold' gutterBottom>
+      <Typography variant="h3" color="primary" fontWeight="bold" gutterBottom>
         {title}
       </Typography>
-      <Typography variant='p'>{instructions}</Typography>
+      <Typography variant="p">{instructions}</Typography>
     </Box>
   );
 };
