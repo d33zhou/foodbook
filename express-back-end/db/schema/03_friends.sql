e20dd02a70d230f32ff5f12514d98d163d0799e7
David Zhou <d33zhou@outlook.com>
2022-02-17 13:02:39 -0500

    update friends schema

--- express-back-end/db/schema/03_friends.sql
+++ express-back-end/db/schema/03_friends.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS friends CASCADE;
 
 CREATE TABLE friends(
   id SERIAL PRIMARY KEY NOT NULL,
-  follow_date DATE NOT NULL,
+  follow_date DATE,
 
   user_id_1 INTEGER REFERENCES users(id) ON DELETE CASCADE,
   user_id_2 INTEGER REFERENCES users(id) ON DELETE CASCADE
