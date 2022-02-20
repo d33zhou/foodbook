aa55765171f23ebdf0bf7822d348a38bcf2b9eeb
Chris Lopes <chris.lopes@gmail.com>
2022-02-19 19:55:55 -0500

    update recipe and bookmarks layout

--- react-front-end/src/components/UserRecipeListItem.jsx
+++ react-front-end/src/components/UserRecipeListItem.jsx
@@ -1,4 +1,10 @@
-import { IconButton, ImageListItem, ImageListItemBar, Box } from '@mui/material';
+import {
+  IconButton,
+  ImageListItem,
+  ImageListItemBar,
+  Box,
+  Typography,
+} from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import { useHistory } from 'react-router-dom';
 
@@ -7,27 +13,44 @@ const UserRecipeListItem = (props) => {
   const history = useHistory();
 
   return (
-    <ImageListItem rows={1} onClick={() => history.push(`/recipe/${id}`)}>
+    <Box sx={{ backgroundColor: '#fff', width: '200px' }}>
       <Box
-        component="img"
+        component='div'
         sx={{
-          height: 1/1,
-          width: 1/1
-        }}
-        src={`${image_link}?w=164&h=164&fit=crop&auto=format`}
-      />
-      <ImageListItemBar
-        title={title}
-        subtitle={creator_id && `by ${first_name} ${last_name[0]}.` || null}
-        actionIcon={creator_id &&
-          <IconButton
-            sx={{ color: 'rgba(255, 255, 255, 054)' }}
-          >
-            <StarIcon />
-          </IconButton>
-        }
-      />
-    </ImageListItem>
+          backgroundImage: `url(${image_link})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          width: '200px',
+          height: '200px',
+        }}>
+        &nbsp;
+      </Box>
+      <Typography variant='h6' sx={{ textAlign: 'left', margin: '1rem' }}>
+        {title}
+      </Typography>
+    </Box>
+
+    // <ImageListItem rows={1} onClick={() => history.push(`/recipe/${id}`)}>
+    //   <Box
+    //     component="img"
+    //     sx={{
+    //       height: 1/1,
+    //       width: 1/1
+    //     }}
+    //     src={`${image_link}?w=164&h=164&fit=crop&auto=format`}
+    //   />
+    //   <ImageListItemBar
+    //     title={title}
+    //     subtitle={creator_id && `by ${first_name} ${last_name[0]}.` || null}
+    //     actionIcon={creator_id &&
+    //       <IconButton
+    //         sx={{ color: 'rgba(255, 255, 255, 054)' }}
+    //       >
+    //         <StarIcon />
+    //       </IconButton>
+    //     }
+    //   />
+    // </ImageListItem>
   );
 };
 
